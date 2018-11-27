57d17420a2bf01db11c7011556741149ca268ee9
Justin Stonebarger <jjstonebarger@gmail.com>
2018-11-26 19:56:26 -0600

    Wu Tang Clan Ain't Nothin' to F&$k Wit!

--- src/org/launchcode/java/demos/java4python/restaurant/Menu.java
+++ src/org/launchcode/java/demos/java4python/restaurant/Menu.java
@@ -1,4 +1,6 @@
 package org.launchcode.java.demos.java4python.restaurant;
 
 public class Menu {
+    private String MenuItem;
+    private int updateDate;
 }
--- src/org/launchcode/java/demos/java4python/restaurant/MenuItem.java
+++ src/org/launchcode/java/demos/java4python/restaurant/MenuItem.java
@@ -1,4 +1,45 @@
 package org.launchcode.java.demos.java4python.restaurant;
 
+// Create a MenuItem class with data fields for various qualities of the class
 public class MenuItem {
+    private Double price;
+    private String description;
+    private String category;
+    private String newness;
+
+
+    // Getters/Setters addition
+
+    private Double getPrice() {
+        return price;
+    }
+
+    private void setPrice(Double aPrice) {
+        price = aPrice;
+    }
+
+    private String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String aDescription){
+        description = aDescription;
+    }
+
+    private String getCategory(){
+        return category;
+    }
+
+    private void setCategory(String aCategory){
+        category = aCategory;
+    }
+
+    private String newness(){
+        return newness;
+    }
+
+    private void setNewness(String aNewness){
+        newness = aNewness;
+    }
+
 }
