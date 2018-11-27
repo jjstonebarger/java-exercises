79cd02d3710990728471df9ac2e8dc6a6ba845b4
Justin Stonebarger <jjstonebarger@gmail.com>
2018-11-26 19:07:41 -0600

    Wu Tang Clan Ain't Nothin' to F&$k Wit!

---
+++ src/org/launchcode/java/demos/java4python/restaurant/Menu.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.demos.java4python.restaurant;
+
+public class Menu {
+}
---
+++ src/org/launchcode/java/demos/java4python/school/Student.java
@@ -0,0 +1,18 @@
+package org.launchcode.java.demos.java4python.school;
+
+
+public class Student {
+
+    private String name;
+    private int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String aName){
+       this.name = aName;
+    }
+}
